782d0462d30836bbc1317262a45eba3881d12555
Rahul250192 <gupta.rahulg25@gmail.com>
2019-02-07 01:18:41 -0700

    directory code

--- model_train.py
+++ model_train.py
@@ -3,4 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys, os, shutil
 
+from layers import LayerTrain
 
+def make_dir(dire):
+
+	if os.path.exists(dire):
+		shutil.rmtree(dire, ignore_errors=True)
+	os.makedirs(dire)
+
+def main():
+	hist_dir = './histograms'
+	weight_dir = './weights'
+
+	make_dir(hist_dir)
+	make_dir(weight_dir)
+
+	#####creating computation graph######
+
+
+
+	#####training#########3
+
+
+if __name__ == "__main__":
+	main()
